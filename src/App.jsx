82e54f434ac15ee7ea33fa2b1913956d515d3c09
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-05-28 11:58:50 -0600

    add signin components and firebase integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './pages/navigation/navigation.component'
 import Home from './pages/home/home.component'
+import SignIn from './pages/signin/signin.component'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Home />} />
+        <Route path='signin' element={<SignIn />} />
       </Route>
     </Routes>
   )
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,50 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBe4-Ul0BLXE2Zptc9RHv1VkVhCoKQ4wDE',
+  authDomain: 'super-store-e24f8.firebaseapp.com',
+  projectId: 'super-store-e24f8',
+  storageBucket: 'super-store-e24f8.appspot.com',
+  messagingSenderId: '81630968328',
+  appId: '1:81630968328:web:b23499ba050bbda4292fbd',
+  measurementId: 'G-23FNDYXDJE',
+}
+
+initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider()
+provider.setCustomParameters({
+  prompt: 'select_account',
+})
+
+export const auth = getAuth()
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid)
+  const userSnapshot = await getDoc(userDocRef)
+
+  if (userSnapshot.exists()) {
+    return userDocRef
+  }
+
+  const { displayName, email } = userAuth
+  const createdAt = new Date()
+
+  try {
+    await setDoc(userDocRef, {
+      displayName,
+      email,
+      createdAt,
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+
+  return userDocRef
+}
