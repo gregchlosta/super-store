76302e0e61c24344f77f7fc992fcdd0ffc9bd807
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-05-28 20:57:48 -0600

    Add sign-in-form and finish authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './pages/navigation/navigation.component'
 import Home from './pages/home/home.component'
-import SignIn from './pages/signin/signin.component'
+import Authentication from './pages/authentication/authentication.component'
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Home />} />
-        <Route path='signin' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   )
