35ad2878f07bdd163503014299bf156e9f60cfac
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-05-30 13:01:53 -0600

    add checkout page and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from './pages/navigation/navigation.component'
 import Home from './pages/home/home.component'
 import Authentication from './pages/authentication/authentication.component'
 import Shop from './pages/shop/shop.component'
+import Checkout from './pages/checkout/checkout.comoponent'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='checkout' element={<Checkout />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
