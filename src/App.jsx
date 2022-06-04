fbc79c9968a7a4cef1e35553a189a5d24fd94f2b
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-06-03 20:21:53 -0600

    Add Styled Components Redux and Redux-Saga,

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import { Routes, Route } from 'react-router-dom'
-import Navigation from './pages/navigation/navigation.component'
-import Home from './pages/home/home.component'
-import Authentication from './pages/authentication/authentication.component'
-import Shop from './pages/shop/shop.component'
-import Checkout from './pages/checkout/checkout.comoponent'
+
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+import Authentication from './routes/authentication/authentication.component'
+import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
+import { checkUserSession } from './store/user/user.action'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(checkUserSession())
+  }, [])
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop/*' element={<Shop />} />
-        <Route path='checkout' element={<Checkout />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   )
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,61 +1,52 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import FormInput from '../form-input/form-input.component'
-import Button from '../button/button.component'
+import FormInput from '../form-input/form-input.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 import {
-  signInWithGooglePopup,
-  signInAuthUserWithEmailAndPassword,
-} from '../../utils/firebase.utils'
-
-import './sign-in-form.styles.scss'
+  googleSignInStart,
+  emailSignInStart,
+} from '../../store/user/user.action';
 
 const defaultFormFields = {
   email: '',
   password: '',
-}
+};
 
 const SignInForm = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields)
-  const { email, password } = formFields
+  const dispatch = useDispatch();
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
 
   const resetFormFields = () => {
-    setFormFields(defaultFormFields)
-  }
+    setFormFields(defaultFormFields);
+  };
 
   const signInWithGoogle = async () => {
-    await signInWithGooglePopup()
-  }
+    dispatch(googleSignInStart());
+  };
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     try {
-      await signInAuthUserWithEmailAndPassword(email, password)
-
-      resetFormFields()
+      dispatch(emailSignInStart(email, password));
+      resetFormFields();
     } catch (error) {
-      switch (error.code) {
-        case 'auth/wrong-password':
-          alert('incorrect password for email')
-          break
-        case 'auth/user-not-found':
-          alert('no user associated with this email')
-          break
-        default:
-          console.log(error)
-      }
+      console.log('user sign in failed', error);
     }
-  }
+  };
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
-    setFormFields({ ...formFields, [name]: value })
-  }
+    setFormFields({ ...formFields, [name]: value });
+  };
 
   return (
-    <div className='sign-up-container'>
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -76,15 +67,19 @@ const SignInForm = () => {
           name='password'
           value={password}
         />
-        <div className='buttons-container'>
+        <ButtonsContainer>
           <Button type='submit'>Sign In</Button>
-          <Button type='button' buttonType='google' onClick={signInWithGoogle}>
-            Google sign in
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            type='button'
+            onClick={signInWithGoogle}
+          >
+            Sign In With Google
           </Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
-  )
-}
+    </SignInContainer>
+  );
+};
 
-export default SignInForm
+export default SignInForm;
