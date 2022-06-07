2d01aed1c9660f22466ab0136e3e2b1482e9fa32
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-06-07 12:14:43 -0600

    add typed cart redux

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,24 +1,29 @@
-import { createSelector } from 'reselect';
+import { createSelector } from 'reselect'
+import { CartItem } from './cart.types'
 
-const selectCartReducer = (state) => state.cart;
+const selectCartReducer = (state: any) => state.cart
 
 export const selectCartItems = createSelector(
   [selectCartReducer],
   (cart) => cart.cartItems
-);
+)
 
 export const selectIsCartOpen = createSelector(
   [selectCartReducer],
   (cart) => cart.isCartOpen
-);
+)
 
 export const selectCartCount = createSelector([selectCartItems], (cartItems) =>
-  cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
-);
+  cartItems.reduce(
+    (total: number, cartItem: CartItem) => total + cartItem.quantity,
+    0
+  )
+)
 
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
-    (total, cartItem) => total + cartItem.quantity * cartItem.price,
+    (total: number, cartItem: CartItem) =>
+      total + cartItem.quantity * cartItem.price,
     0
   )
-);
+)
